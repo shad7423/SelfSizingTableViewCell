10269d0afbc9426a403a012b9311db8a82405f7c
Shadab Khan <shadabkhan@shadabs-mac-mini.local>
2018-02-28 11:14:44 +0530

    selfSizingTableViewCell

---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by Shadab Khan on 2/27/18.
+//  Copyright © 2018 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+
+
+}
---
+++ Artistry/artistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  artistTableViewCell.swift
+//  Artistry
+//
+//  Created by Shadab Khan on 2/27/18.
+//  Copyright © 2018 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class artistTableViewCell: UITableViewCell {
+
+      @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+  
+}
